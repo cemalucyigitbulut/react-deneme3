34f8bdd72558d989aa374b1eaf04587aa99f5188
YilanCemal <cemalyigit123@hotmail.com>
2023-02-09 18:03:00 +0300

    düzeltme

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -8,19 +8,49 @@ const AddItem = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setItems([...items,{name:itemName, price:itemPrice}])
-    setItemName("")
-    setItemPrice("")
-  }
+
+    if (itemName === "" || itemPrice === "") {
+      alert("enter both the item and the price");
+      return;
+    }
+
+    setItems([...items, { name: itemName, price: itemPrice }]);
+    setItemName("");
+    setItemPrice("");
+  };
+
+  const handleNameChange = (event) => {
+    const name = event.target.value;
+    if (typeof name === "string") {
+      setItemName(name);
+    }
+  };
+
+  const handlePriceChange = (event) => {
+    const price = parseFloat(event.target.value);
+    if (!isNaN(price)) {
+      setItemPrice(price);
+    }
+  };
 
   return (
     <>
       <h1>Add Items</h1>
       <form onSubmit={handleSubmit}>
         <p>Fill the parts before adding item</p>
-        <input type="text" placeholder="Item Name" value={itemName}  onChange={(event) => setItemName(event.target.value)} />
+        <input
+          type="text"
+          placeholder="Item Name"
+          value={itemName}
+          onChange={handleNameChange}
+        />
         <br />
-        <input type="text" placeholder="Item Price" value={itemPrice} onChange={(event) => setItemPrice(event.target.value)}/>
+        <input
+          type="text"
+          placeholder="Item Price"
+          value={itemPrice}
+          onChange={handlePriceChange}
+        />
         <br />
         <button type="submit">Add Item</button>
       </form>
